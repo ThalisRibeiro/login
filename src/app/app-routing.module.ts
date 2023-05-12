963a22d08eef3f62b6dac6c7e3183fe109718ca4
ThalisRibeiro <thalis199@outlook.com>
2023-05-12 12:52:36 -0300

    Adicionando LoginArea as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAreaComponent } from './login-area/login-area.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginAreaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
