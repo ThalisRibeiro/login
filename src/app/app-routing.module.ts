56a52155d1d6986711a1ea459d6d327e9df78d26
ThalisRibeiro <thalis199@outlook.com>
2023-05-12 13:31:52 -0300

    Criada rota para esqueci senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { LoginAreaModule } from './login-area/login-area.module';
 import { LoginAreaComponent } from './login-area/login-area.component';
+import { EsqueciSenhaComponent } from './login-area/esqueci-senha/esqueci-senha.component';
 
 const routes: Routes = [
-  {path:'', component: LoginAreaComponent}
+  {path:'', component: LoginAreaComponent},
+  {path:'forgotpassword', component: EsqueciSenhaComponent}
+  // {path:'', loadChildren: ()=>LoginAreaModule}
+  
 ];
 
 @NgModule({
--- src/app/login-area/login-area.module.ts
+++ src/app/login-area/login-area.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatButtonModule} from '@angular/material/button'; 
 
 import { LoginAreaComponent } from './login-area.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
 
 
 
 @NgModule({
-  declarations: [LoginAreaComponent],
+  declarations: [LoginAreaComponent, EsqueciSenhaComponent],
   imports: [
     CommonModule,
+    AppRoutingModule,
     MatInputModule,
     MatButtonModule
   ],
   exports:[
-    LoginAreaComponent
+    LoginAreaComponent,
+    EsqueciSenhaComponent
   ]
 })
 export class LoginAreaModule { }
