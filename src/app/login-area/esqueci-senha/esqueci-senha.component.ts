c7a6f30c70c58fb376b929ab2addd6ffac4776c6
ThalisRibeiro <thalis199@outlook.com>
2023-05-13 11:50:41 -0300

    Criando um reactive form

--- src/app/login-area/esqueci-senha/esqueci-senha.component.ts
+++ src/app/login-area/esqueci-senha/esqueci-senha.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-esqueci-senha',
   templateUrl: './esqueci-senha.component.html',
   styleUrls: ['./esqueci-senha.component.css']
 })
-export class EsqueciSenhaComponent {
-
+export class EsqueciSenhaComponent implements OnInit {
+ formulario: FormGroup;
+ 
+ constructor(private formBuilder: FormBuilder) {
+  this.formulario = this.formBuilder.group({
+    email:[null]
+   }) 
+  }
+  ngOnInit(): void {
+   
+ }
+ logForm(){
+  console.log(this.formulario.value)
+ }
 }
